dc31fe72b33c49fa377dbcb2f13b81f696d66d20
brnkgabriel <brnkgabriel@gmail.com>
2020-04-03 07:33:25 +0100

    cleancode version 6

--- public/common-js/util.js
+++ public/common-js/util.js
@@ -20,17 +20,60 @@ class FormatPath {
   }
 }
 
-function setAttributes (el, obj) {
-  Object.keys(obj).forEach(key => el.setAttribute(key, obj[key]))
+// How to use BuildElement
+// var prop = {
+//   sku: ['div', { class: '-sku' }, { style: 'background-color:yellow;' }, 'this is a div']
+// }
+
+// var sku = new BuildElement(prop['sku'])
+//   .create()
+//   .setAttributes()
+//   .setStyle()
+//   .setHTML()
+//   .getElement()
+class BuildElement {
+  constructor(properties) {
+    this.tag = properties[0]
+    this.attributes = properties[1]
+    this.styles = properties[2]
+    this.textContent = properties[3]
+    this.element = null
+  }
+
+  create() {
+    this.element = document.createElement(this.tag)
+    return this
+  }
+
+  assignAttribute(object) {
+    var keys = Object.keys(object)
+    keys.forEach(key => {
+      var value = object[key]
+      this.element.setAttribute(key, value)
+    })
+  }
+
+  setAttributes() {
+    this.assignAttribute(this.attributes)
+    return this
+  }
+
+  setStyle() {
+    this.assignAttribute(this.styles)
+    return this
+  }
+
+  setHTML() {
+    this.element.innerHTML = this.textContent
+    return this
+  }
+
+  getElement() {
+    return this.element
+  }
+
 }
 
-function create(props) {
-  var tag = props[0], attributes = props[1], styles = props[2], textContent = props[3]
-  var el = document.createElement(tag); setAttributes(el, attributes);
-  setAttributes(el, styles); textContent ? el.innerHTML = textContent : ''
-  return el
-}
-
-function appendMany2One (many, one) {
+function appendMany2One(many, one) {
   many.forEach(each => one.appendChild(each))
 }
